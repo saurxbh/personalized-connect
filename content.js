7f9e895bf58abc020257c4065ea53183941a5733
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-10-25 00:04:22 -0400

    Add custom messages with placeholder text

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ import handleConnectionRequest from "./utils/handleConnectionRequest";
 
 // Inject "Send Connection Request" button next to Connect/Message button
 function injectButton() {
-    const buttonParent   = document.querySelector(".pvs-profile-actions__action button") || document.querySelector(".entry-point.profile-action-compose-option");
+    const buttonParent = document.querySelector(".entry-point.profile-action-compose-option");
 
     
     if (buttonParent && !document.getElementById('send-custom-connection')) {
@@ -18,25 +18,6 @@ function injectButton() {
     }
   }
   
-  
-  
-  // Generate a personalized note based on the person's profile
-  function generatePersonalizedNote() {
-    let note = "Hi, I'd like to connect!";
-    
-    // Extract school and company information (mock data for now)
-    const school = document.querySelector('.pv-entity__school-name')?.innerText;
-    const company = document.querySelector('.pv-entity__company-summary-info')?.innerText;
-    
-    if (school) {
-      note = `Hi, I noticed you studied at ${school}. I'd love to connect and learn more about your experience!`;
-    } else if (company) {
-      note = `Hi, I see you worked at ${company}. I'd be excited to connect and exchange ideas!`;
-    }
-  
-    return note;
-  }
-  
-  // Run the script when the page loads
-  window.onload = injectButton();
+// Run the script when the page loads
+window.onload = injectButton();
   
